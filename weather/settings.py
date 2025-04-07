cdeaf494e78b1830104e215751bc5a30a2048ef6
Heaknou <heaknou.h@bizsolution.com.kh>
2025-04-07 11:56:23 +0700

    allow host

--- weather/settings.py
+++ weather/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-gf-++mt4yno9o5shm&1s_jfaf&ki0ut4@8a53w@!f^h7&#q=ha
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['djangoweatherapp.up.railway.app', 'localhost', '127.0.0.1']
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'djangoweatherapp.up.railway.app']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.up.railway.app']
-
+ALLOWED_HOSTS = ['djangoweatherapp.up.railway.app', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://djangoweatherapp.up.railway.app']
 
